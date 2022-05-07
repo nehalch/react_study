8206ccf099e30672eb020aff81182118f312df13
Andrew <rtpdfv@yahoo.com>
2022-05-07 23:16:08 +0200

    .

--- 0/index.js
+++ 0/index.js
@@ -1,11 +1,1 @@
-import React from "react";
-import { ReactDOM } from "react-dom";
-
-const element = (
-    <div>
-        <p>123</p>
-        <h1 className="123">123123</h1>
-    </div>
-);
-
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<p>123</p>, document.getElementById("root"));
