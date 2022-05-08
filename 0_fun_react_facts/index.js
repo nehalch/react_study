8222563ebabf94791038c54db7e2bee12b44472d
Andrew <rtpdfv@yahoo.com>
2022-05-08 12:15:30 +0200

    remake it again.

---
+++ 0_fun_react_facts/index.js
@@ -0,0 +1,15 @@
+const Page = () => {
+    return (
+        <div>
+            <img src="./react.png" alt="React logo" width="40px"/>
+            <h1>Facts about react</h1>
+            <ol>
+                <li>Whole site in one react app.</li>
+                <li>Cross platform apps.</li>
+                <li>Easy to use.</li>
+            </ol>
+        </div>
+    );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
