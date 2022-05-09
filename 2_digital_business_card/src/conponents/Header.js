3325c40f29e924b5cc28583f0a2e4a4193ca0484
Andrew <rtpdfv@yahoo.com>
2022-05-09 02:32:55 +0200

    some changes and 1 fixme

--- 2_digital_business_card/src/conponents/Header.js
+++ 2_digital_business_card/src/conponents/Header.js
@@ -8,8 +8,8 @@ export default function Header() {
             <img src={Homer} alt="Homer Simpson" width="300" />
             <h1>Homer Simpson</h1>
             <h3>FORTRAN Developer</h3>
-            <h5><a href="google.com">google.com</a></h5>
-            <a href="mailto:email@example.com"><button>mail</button></a>
+            <h5><a href="https://google.com">google.com</a></h5>
+            <a href="mailto:email@example.com"><button>Mail Me</button></a>
 
         </header>
     );
