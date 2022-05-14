7a515197d15ba402e03faf74b551fef22d4b65ea
Andrew <rtpdfv@yahoo.com>
2022-05-14 16:04:59 +0000

    .

--- 3_airbnb_experiences_clone/src/conmponents/Card.js
+++ 3_airbnb_experiences_clone/src/conmponents/Card.js
@@ -3,13 +3,15 @@ import React from "react";
 export default function Card(product) {
     return (
         <div className="card">
-            <img src={process.env.PUBLIC_URL + "/images/products/" + product.photo} alt="Product card." />
-            <h2>{product.name}</h2>
-            <p> {product.quantity} pcs.</p>
-            <p> {product.price} USD</p>
-            <p> {product.rate} stars</p>
-            <p> {product.country} </p>
-            <p> Description: {product.description} </p>
+            <div className="card_photo">
+                <img className="card" src={process.env.PUBLIC_URL + "/images/products/" + product.photo} alt="Product card." />
+            </div>
+            <h2 className="card" >{product.name}</h2>
+            <p className="card" > {product.quantity} pcs.</p>
+            <p className="card" > {product.price} USD</p>
+            <p className="card" > {product.rate} stars</p>
+            <p className="card" > {product.country} </p>
+            <p className="card" > Description: {product.description} </p>
         </div>
     )
 };
