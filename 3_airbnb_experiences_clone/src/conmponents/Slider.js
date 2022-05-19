66b338d96049491ce4779f5537b72c854a4d2ac2
Andrew <rtpdfv@yahoo.com>
2022-05-19 18:51:49 +0200

    .

--- 3_airbnb_experiences_clone/src/conmponents/Slider.js
+++ 3_airbnb_experiences_clone/src/conmponents/Slider.js
@@ -3,15 +3,6 @@ import React from "react";
 import Card from "./Card";
 import items from "../data/items";
 
-// const json = '{{"result":true, "count":42},{"result":true, "count":42}}';
-// const obj = JSON.parse(json);
-
-// console.log(obj.count);
-// // expected output: 42
-
-// console.log(obj.result);
-// expected output: true
-
 export default function Slider() {
     const cards = items.map(item => {
         return <Card
