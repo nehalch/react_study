f753a63a8f4e5d87280eead3d5aa8c7d4364dd69
Andrew <rtpdfv@yahoo.com>
2022-05-19 19:15:20 +0200

    .

---
+++ 4_travel_journal/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/App.css";
+
+import Header from "./conmponents/Header";
+import MainContent from "./conmponents/MainContent";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
