80da1eacbc30ca04ea7042065127ed651740dcb8
Andrew <rtpdfv@yahoo.com>
2022-06-07 02:00:27 +0200

    settings

--- 4_travel_journal/src/components/MainContent.js
+++ 4_travel_journal/src/components/MainContent.js
@@ -3,29 +3,25 @@ import React from "react";
 import items from "../data/items";
 import Card from "./Card";
 
-
-const cards_spacer = (cards) => { 
-    let cards_with_spaces = [];
-    for (let card of cards) {
-        if (card === cards[cards.length-1]) {
-            cards_with_spaces.push(card);
-        } else {
-            cards_with_spaces.push(card,<hr/>);            
-        }
-    }
-    return cards_with_spaces;
-}
-
+const cards_spacer = (cards) => {
+	let cards_with_spaces = [];
+	for (let card of cards) {
+		if (card === cards[cards.length - 1]) {
+			cards_with_spaces.push(card);
+		} else {
+			cards_with_spaces.push(card, <hr />);
+		}
+	}
+	return cards_with_spaces;
+};
 
 const MainContent = () => {
-    let cards = cards_spacer(items.map(item => <Card {...item} />));
-    return (
-        <main>
-            <div className="cards_box">
-                {cards}
-            </div>
-        </main>
-    );
-}
+	let cards = cards_spacer(items.map((item) => <Card {...item} />));
+	return (
+		<main>
+			<div className="cards_box">{cards}</div>
+		</main>
+	);
+};
 
 export default MainContent;
